6deaa4003eda6c4784e20ceb39b5cf85e7dfc27f
Sergey <sp.polozov@gmail.com>
2019-12-03 20:12:12 +0100

    Updates

---
+++ precache-manifest.025008ad333e3a8bf53abab9ae6dfced.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4454026cdb60ed6a6f256216a2e2ec",
+    "url": "/vk-sw-tree/index.html"
+  },
+  {
+    "revision": "0bab2aaebe75e6e31bfa",
+    "url": "/vk-sw-tree/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "d496a673fd4b49e61af5",
+    "url": "/vk-sw-tree/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "0bab2aaebe75e6e31bfa",
+    "url": "/vk-sw-tree/static/js/2.ae9e17d7.chunk.js"
+  },
+  {
+    "revision": "d496a673fd4b49e61af5",
+    "url": "/vk-sw-tree/static/js/main.1a83698b.chunk.js"
+  },
+  {
+    "revision": "cbad066ae94565f1eade",
+    "url": "/vk-sw-tree/static/js/runtime-main.5007d2d3.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-sw-tree/static/media/persik.4e1ec840.png"
+  }
+]);
