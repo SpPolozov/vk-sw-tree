9ee1772c3673c43e818e250a4aec717eb957c08d
Sergey Polozov <serg.polozov@gmail.com>
2019-12-04 11:58:04 +0300

    changes

--- src/panels/Epic.js
+++ src/panels/Epic.js
@@ -1,3 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { platform, IOS } from '@vkontakte/vkui';
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderButton';
+import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
+import Icon24Back from '@vkontakte/icons/dist/24/back';
+
 class Epic extends React.Component {
     constructor (props) {
         super(props);
