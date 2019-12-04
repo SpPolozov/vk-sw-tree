966e2d96054e41b885c42c02192d5ef5fa917517
Sergey Polozov <serg.polozov@gmail.com>
2019-12-04 20:09:21 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -98,6 +98,7 @@ class App extends React.Component {
 	};
 
 	render () {
+		connect.send("VKWebAppInit", {});
 		const modal = (
 			<ModalRoot activeModal={this.state.activeModal}>
 				<ModalCard
