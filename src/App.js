8b3acbb2d7ce23e824466265338e78b09d9c8e67
Sergey Polozov <serg.polozov@gmail.com>
2019-12-04 15:14:30 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 import '@vkontakte/vkui/dist/vkui.css';
 import { View, Panel, PanelHeader, Group, List, Cell, Tabbar, TabbarItem, Epic} from '@vkontakte/vkui';
-import { Icon28Newsfeed, Icon28Search} from '@vkontakte/icons/dist/28/chevron_back';
+import Icon28Newsfeed from '@vkontakte/icons/dist/28/chevron_back';
+import Icon28Search from '@vkontakte/icons/dist/28/chevron_back';
 
 class App extends React.Component {
 	constructor (props) {
@@ -25,8 +26,9 @@ class App extends React.Component {
 						onClick={this.onStoryChange}
 						selected={this.state.activeStory === 'feed'}
 						data-story="feed"
-						text="Новости">
-						<Icon28Newsfeed/>
+						text="Новости"
+					>
+						<Icon28Newsfeed />
 					</TabbarItem>
 					<TabbarItem
 						onClick={this.onStoryChange}
